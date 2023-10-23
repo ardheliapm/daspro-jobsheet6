8ebaca040325a4ea4b4d23ed627c4323d15b2923
dedelduwell <ardheliamputri@gmail.com>
2023-10-23 14:31:58 +0700

    Modifikasi Kedua

--- Pemilihan2Percobaan105.java
+++ Pemilihan2Percobaan105.java
@@ -10,12 +10,26 @@ public class Pemilihan2Percobaan105 {
 
         System.out.print("Masukkan Tahun :");
         int Tahun = Absen.nextInt();
-            if ((Tahun % 4) == 0 && ((Tahun % 100) != 0 && ((Tahun % 400) == 0 ))) {
+           
+        boolean Kabisat = true;
+
+        if ((Tahun % 4) == 0) {
+            Kabisat = true;
+                if ((Tahun % 100 ) == 0) {
+                    Kabisat = (Tahun % 400) == 0;
+                }
+            } 
+        if (Kabisat) {
+            System.out.println("Merupakan Tahun Kabisat");
+        } else {
+            System.out.println("Bukan Merupakan Tahun Kabisat");
+        }
+            /*if ((Tahun % 4) == 0 && ((Tahun % 100) != 0 && ((Tahun % 400) == 0 ))) {
                 //if ((Tahun % 100) != 0 && ((Tahun % 400) != 0 ))
                 System.out.println("Tahun Kabisat");
             } else 
                 System.out.println("Bukan Tahun Kabisat");
-
+            /* */
 
     }
 }
